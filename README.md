03ca5241639b950ea17d7b6ecfa996c4456d458d
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2017-11-01 03:39:26 +0530

    Created the Projects page
    * Update the bootstrap, jquery & MDB source files
    
    * Added font links in CSS
    
    * Template of projects page

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pkd2512.github.io
-Personal website
+Personal Portfolio website 
---
+++ projects/projects.js
@@ -0,0 +1,41 @@
+if ('content' in document.createElement('template')) {
+  console.log('Templates are supported');
+
+// List of all projects and related data
+var projects = [
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']}
+];
+console.log(projects);
+// Get a reference to the comments list in the main DOM.
+var projectsList = document.getElementById('projects');
+
+// Loop through each of the projects and add them to the projects list.
+for (var i = 0; i < projects.length; i++) {
+  var project = projects[i];
+  var tmpl = document.getElementById('card-template').content.cloneNode(true);
+  tmpl.querySelector('.img-fluid').src = project.location;
+  tmpl.querySelector('.card-title').innerText = project.title;
+  tmpl.querySelector('.card-text').innerText = project.body;
+  tmpl.querySelector('.target').href = project.target;
+  for (var t=0; t < project.tags.length; t++) 
+  {
+  	var tag = document.createElement('span');
+  	tag.innerText = project.tags[t];
+  	tag.setAttribute('class','tags text-font');
+  	tmpl.querySelector('.tags-row').appendChild(tag);
+  }
+  projectsList.appendChild(tmpl);
+}
+
+} 
+else {
+  console.log('Templates are not supported');
+}
+
