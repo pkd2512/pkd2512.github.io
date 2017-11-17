293652155938fd5941a61e156f39db08f75c071f
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2017-11-17 08:01:37 +0530

    Added dodata project + fixes
    * Added navbar, backtotop button
    * Localised files

--- projects/projects.js
+++ projects/projects.js
@@ -1,16 +1,16 @@
 if ('content' in document.createElement('template')) {
   console.log('Templates are supported');
-
+new WOW().init();
 // List of all projects and related data
 var projects = [
-  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
-  {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
+  {'location': 'thumbs/demo.jpeg', 'title': 'DoData', 'body': 'Making knowledge available on the internet accessible to the underprivileged', 'target': '/projects/dodata/', 'tags':['UI Design','Visual Identity']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']},
   {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']}
+ // {'location': 'thumbs/demo.jpeg', 'title': 'Card title', 'body': 'Some quick example content.', 'target': 'https://google.com', 'tags':['a','b','c']}
 ];
 console.log(projects);
 // Get a reference to the comments list in the main DOM.
@@ -33,7 +33,6 @@ for (var i = 0; i < projects.length; i++) {
   }
   projectsList.appendChild(tmpl);
 }
-
 } 
 else {
   console.log('Templates are not supported');
