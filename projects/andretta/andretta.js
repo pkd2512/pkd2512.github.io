b3e513d46b8b2fafb4da8f96ec0129493bcdd978
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2017-11-21 21:12:59 +0530

    Projects page updated

---
+++ projects/andretta/andretta.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // alert("Ready!");
+    $('.se-pre-con').fadeOut("slow");
+}, false);
+
+new WOW().init();
+    
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if ($(window).innerWidth() < 768) 
+    {
+        $("#top").hide();
+    }
+    else {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("top").style.display = "block";
+        } 
+        else {
+            document.getElementById("top").style.display = "none";
+        }
+    }
+    
+}
+// Update copyright with current year
+var d = new Date();
+$("footer #date").text(d.getFullYear()); 
+
+// When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+//     document.documentElement.scrollTop = 0; // For IE and Firefox
+// }
+
+
+
