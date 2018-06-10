26e9fa0a3f1638978b5ae4a4b0a8b6c7a198bf39
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2018-06-11 01:18:40 +0530

    soulace design brief section

--- projects/soulace/soulace.js
+++ projects/soulace/soulace.js
@@ -23,6 +23,13 @@ function scrollFunction() {
     }
     
 }
+// (function() { var v = document.getElementsByClassName("youtube-player"); for (var n = 0; n < v.length; n++) { v[n].onclick = function () { var iframe = document.createElement("iframe"); iframe.setAttribute("src", "//www.youtube.com/embed/" + this.dataset.id + "?autoplay=1&mute=1&autohide=2&border=0&wmode=opaque&enablejsapi=1&rel="+ this.dataset.related +"&controls="+this.dataset.control+"&showinfo=" + this.dataset.info); iframe.setAttribute("frameborder", "0"); iframe.setAttribute("id", "youtube-iframe"); iframe.setAttribute("style", "width: 100%; height: 100%; position: absolute; top: 0; left: 0;"); if (this.dataset.fullscreen == 1){ iframe.setAttribute("allowfullscreen", ""); } while (this.firstChild) { this.removeChild(this.firstChild); } this.appendChild(iframe); }; } })();
+
+// web-video play
+// function webPlay() {
+//     document.getElementById('web-video').innerHTML = '<iframe width="960" height="540" src="images/soulace-web.mp4" frameborder="0"></iframe>';
+// }
+
 /* Image protection code */
 
 // Disable dragging of images
