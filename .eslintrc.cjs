7c4eec9f13427bb686a1ecce65f4bbf1246f8ec5
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2023-07-08 13:46:23 +0530

    test build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
 	root: true,
 	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+	plugins: ['svelte3', '@babel', 'mocha'],
+	parser: '@babel/eslint-parser',
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
@@ -11,4 +13,36 @@ module.exports = {
 		es2017: true,
 		node: true
 	}
+	rules: {
+    indent: ['error', 2],
+    semi: ['error', 'always'],
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    'operator-linebreak': ['error', 'after'],
+    'space-before-function-paren': ['error', 'never'],
+    eqeqeq: 'warn',
+    'no-unused-vars': ['warn', { args: 'after-used' }],
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+      rules: {
+        'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 2 }],
+        'import/first': 'off',
+        'import/no-duplicates': 'off',
+        'import/no-mutable-exports': 'off',
+        'import/no-unresolved': 'off',
+        indent: ['error', 2],
+      },
+    },
+  ],
 };
