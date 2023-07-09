aa45c474cb88ddc8fcc38120b1266c6a986c32d9
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2023-07-09 19:43:18 +0530

    more config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+import img from '$assets/tree.jpg'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img src="{img}" alt="">
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
+  build: { target: 'es2015' },
+  server: {
+    open: true,
+    port: 3000,
+    fs: {
+      allow: ['.'],
+    },
+  },
+  resolve: {
+    alias: {
+      $utils: '/src/utils',
+      $pkg: '/package.json',
+      $assets: '/src/assets',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: { includePaths: ['src', 'node_modules'], quietDeps: true },
+    },
+  },
 });
