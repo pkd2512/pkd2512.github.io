8b7a069833fc18cb5b853e7fec71659b7535c586
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2023-12-30 16:06:13 +0530

    desktop mapbox photostory

--- src/lib/components/custom/projects/AndrettaMap/Map.svelte
+++ src/lib/components/custom/projects/AndrettaMap/Map.svelte
@@ -1,34 +1,191 @@
 <script>
+  // @ts-nocheck
   import { Map } from 'mapbox-gl';
   import 'mapbox-gl/dist/mapbox-gl.css';
-  import { onMount, onDestroy } from 'svelte';
+  import { onMount, afterUpdate, onDestroy } from 'svelte';
   import { env } from '$env/dynamic/public';
 
-  // @ts-ignore
-  let map;
   /**
-   * @type {HTMLDivElement}
+   * @param {String} chapter Name of active chapter
    */
-  let mapContainer;
-  let lng, lat, zoom;
+  export let activeChapter = '';
 
+  $: console.log(activeChapter);
+
+  let map;
+  let mapContainer;
+  let chapters;
+  let windowWidth = 0;
+
+  // Map props corresponding to the places chapters for desktop and mobile
+  $: if (windowWidth < 768) {
+    chapters = {
+      Opening: {
+        bearing: 0,
+        center: [75.67555805096436, 19.16551504383851],
+        zoom: 3.2,
+        pitch: 45,
+        maxZoom: 5,
+      },
+      Amritsar: {
+        bearing: -35,
+        center: [75.68727914869692, 31.9584518165087],
+        zoom: 6.5,
+        pitch: 45,
+        maxZoom: 12.1,
+      },
+      Palampur: {
+        bearing: -35,
+        center: [76.61724697513705, 32.07697236483118],
+        zoom: 10,
+        pitch: 45,
+        maxZoom: 12,
+        speed: 0.69,
+      },
+      Andretta: {
+        bearing: 21,
+        center: [76.56561167753148, 32.03723247076803],
+        zoom: 15.4,
+        pitch: 60,
+        maxZoom: 16.5,
+        speed: 0.69,
+      },
+      'Wah Tea Estate': {
+        bearing: 28.8,
+        center: [76.55348119511552, 32.091809775256934],
+        zoom: 12,
+        pitch: 45,
+        maxZoom: 14,
+        speed: 0.69,
+      },
+      Ashapuri: {
+        bearing: 9.6,
+        center: [76.7056679896466, 31.85956834559414],
+        zoom: 10,
+        pitch: 35,
+        maxZoom: 11.5,
+        speed: 0.69,
+      },
+      Baijnath: {
+        bearing: 0,
+        center: [76.63416322088835, 32.069179195570115],
+        zoom: 12.2,
+        pitch: 60,
+        maxZoom: 13.5,
+        speed: 0.69,
+      },
+      'Bir-Billing': {
+        bearing: 40,
+        center: [76.7301326957695, 32.05757235949123],
+        zoom: 11.5,
+        pitch: 40,
+        maxZoom: 13,
+        speed: 0.69,
+      },
+    };
+  } else {
+    chapters = {
+      Opening: {
+        bearing: 0,
+        center: [78.365309, 20.225282],
+        zoom: 5,
+        pitch: 45,
+        maxZoom: 5,
+      },
+      Amritsar: {
+        bearing: -35,
+        center: [75.92305318958245, 31.74288500970745],
+        zoom: 8.2,
+        pitch: 45,
+        maxZoom: 12.1,
+      },
+      Palampur: {
+        bearing: -35,
+        center: [76.64597012574859, 32.07353259002102],
+        zoom: 11,
+        pitch: 45,
+        maxZoom: 12,
+        speed: 0.69,
+      },
+      Andretta: {
+        bearing: 21,
+        center: [76.56615374230614, 32.03766792690715],
+        zoom: 16.3,
+        pitch: 60,
+        maxZoom: 16.5,
+        speed: 0.69,
+      },
+      'Wah Tea Estate': {
+        bearing: 28.8,
+        center: [76.55468340701486, 32.08059484693874],
+        zoom: 13.6,
+        pitch: 45,
+        maxZoom: 14,
+        speed: 0.69,
+      },
+      Ashapuri: {
+        bearing: 9.6,
+        center: [76.6972066282583, 31.84099457082523],
+        zoom: 11.4,
+        pitch: 35,
+        maxZoom: 11.5,
+        speed: 0.69,
+      },
+      Baijnath: {
+        bearing: 0,
+        center: [76.63541299172641, 32.08095014508136],
+        zoom: 12.5,
+        pitch: 60,
+        maxZoom: 13.5,
+        speed: 0.69,
+      },
+      'Bir-Billing': {
+        bearing: 40,
+        center: [76.73028711948291, 32.05158797135131],
+        zoom: 12.3,
+        pitch: 40,
+        maxZoom: 13,
+        speed: 0.69,
+      },
+    };
+  }
+
+  // Draw the map
   onMount(() => {
     map = new Map({
       container: mapContainer,
       accessToken: env.PUBLIC_MAPBOX_TOKEN,
-      style: 'mapbox://styles/pkddapacific/cjftgscmo2pzt2rpgjky1aykh',
-      center: [80.67555805096436, 19.16551504383851],
-      zoom: 5,
-      bearing: 0,
-      pitch: 45,
+      style: 'mapbox://styles/pkddapacific/clqrro3qz00tu01qych922aj6',
+      center: chapters.Opening.center,
+      zoom: chapters.Opening.zoom,
+      bearing: chapters.Opening.bearing,
+      pitch: chapters.Opening.pitch,
+      maxZoom: chapters.Opening.maxZoom,
       minZoom: 3,
     });
+  });
+
+  afterUpdate(() => {
+    if (map) map.resize();
   });
 
   onDestroy(() => {
     if (map) map.remove();
   });
+
+  // Update map using fly to
+  const updateMap = (props) => {
+    if (!props) return;
+    map.setMaxZoom(props.maxZoom);
+    map.flyTo(props);
+  };
+
+  $: if (activeChapter && map) {
+    updateMap(chapters[activeChapter] || '');
+  }
 </script>
+
+<svelte:window bind:innerWidth="{windowWidth}" />
 
 <div class="map" bind:this="{mapContainer}"></div>
 
--- src/lib/components/custom/projects/AndrettaMap/index.svelte
+++ src/lib/components/custom/projects/AndrettaMap/index.svelte
@@ -3,28 +3,55 @@
   import Scroller from '@sveltejs/svelte-scroller';
   import Container from '$lib/components/ui/Container/index.svelte';
   import Map from './Map.svelte';
+  import places from './places';
+
+  import { assets } from '$app/paths';
 
   let index = 0,
     offset = 0,
     progress = 0;
+
+  $: activeChapter = places[index]?.hed || '';
+
+  $: console.log(index);
 </script>
 
 <section id="travels">
   <Scroller
     top="{0}"
     bottom="{1}"
-    threshold="{0.75}"
+    threshold="{0.1}"
     query=".container-sm"
     bind:index="{index}"
     bind:offset="{offset}"
     bind:progress="{progress}"
   >
     <div slot="background">
-      <Map />
+      <Map bind:activeChapter="{activeChapter}" />
     </div>
 
     <div slot="foreground">
-      <Container width="sm">hi</Container>
+      {#each places as place}
+        <Container width="sm" id="{place.hed}">
+          {#if place.hed}
+            <div class="h4">{place.hed}</div>
+          {/if}
+
+          {#if place.dek}
+            <p>{place.dek}</p>
+          {/if}
+
+          {#if place.img}
+            <figure>
+              <img
+                src="{assets}/media/projects/andretta/{place.img}"
+                alt="{place.alt}"
+              />
+              <figcaption>{place.caption}</figcaption>
+            </figure>
+          {/if}
+        </Container>
+      {/each}
     </div>
   </Scroller>
 </section>
@@ -34,19 +61,43 @@
 
   #travels {
     margin-block-end: var(--space-xl);
+
+    :global(svelte-scroller-foreground) {
+      pointer-events: none;
+    }
   }
 
   [slot='background'] {
     overflow: hidden;
+    // width: calc(100% - (var(--sm) + var(--space-3xl)));
+    // box-shadow: var(--shadow-2);
   }
 
   [slot='foreground'] {
-    :global(.container-sm) {
-      @include fullheight(1.5);
+    width: calc(var(--sm) + var(--space-3xl));
+    margin-left: auto;
+    margin-right: var(--space-xs);
 
+    :global(figure) {
+      margin-block-start: 0;
+    }
+
+    :global(.container-sm) {
+      background-color: var(--white-soft);
+      box-shadow: var(--shadow-2);
+      padding-inline: var(--space-xl);
+      padding-block-start: var(--space-l);
       display: flex;
-      justify-content: center;
-      align-items: flex-start;
+      flex-direction: column;
+    }
+
+    :global(.container-sm#Opening) {
+      visibility: hidden;
+      @include fullheight;
+    }
+    :global(.container-sm#Closing) {
+      visibility: hidden;
+      @include fullheight;
     }
   }
 </style>
