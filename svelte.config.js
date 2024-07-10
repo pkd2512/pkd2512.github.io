fb7a0605a1b35a6be47b6f2636c38847606721cc
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2024-07-10 09:08:38 +0100

    update readme

--- svelte.config.js
+++ svelte.config.js
@@ -46,10 +46,6 @@ const config = {
       fallback: undefined,
       precompress: false,
       strict: true,
-      // paths: {
-      //   base:
-      //     process.env.NODE_ENV === 'production' ? '/prasantakrdutta.com' : '',
-      // },
     }),
     alias: {
       $pkg: path.resolve('package.json'),
