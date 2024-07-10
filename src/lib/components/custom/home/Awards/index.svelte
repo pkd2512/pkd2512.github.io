8d0c8088427f595ac870402c1439c947f1254b72
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2024-07-10 09:59:32 +0100

    Update Awards component for mobile

--- src/lib/components/custom/home/Awards/index.svelte
+++ src/lib/components/custom/home/Awards/index.svelte
@@ -2,81 +2,60 @@
   import Container from '$lib/components/ui/Container/index.svelte';
   import Marquee from 'svelte-fast-marquee';
   import { assets } from '$app/paths';
+  // @ts-ignore
+  import awards from '$contents/data/awards.csv';
+
   let play = true;
 
   /**
    * @type {number}
    */
   let windowWidth;
+
+  let slice = awards.length / 2;
 </script>
 
 <svelte:window bind:innerWidth="{windowWidth}" />
 
 <section id="awards">
   <Container width="fluid">
-    <Marquee
-      pauseOnHover="{true}"
-      speed="{windowWidth <= 480 ? 25 : 45}"
-      play="{play}"
-    >
-      <a
-        target="_blank"
-        href="https://snd.org/best-of-design-competitions/snd44-annual-creative-competition-results/"
-        ><img
-          src="{assets}/media/awards/snd.png"
-          alt="Society for News Design Awards"
-        /></a
-      >
-      <a
-        target="_blank"
-        href="https://www.reutersagency.com/en/about/journalists-of-the-year-awards/"
-        ><img
-          src="{assets}/media/awards/reuters.jpg"
-          alt="Reuters Journalists of the Year Awards"
-        /></a
-      >
-      <a
-        target="_blank"
-        href="https://winners.webbyawards.com/2021/websites-and-mobile-sites/features-design/technical-achievement/174760/covid-tracker"
-        ><img src="{assets}/media/awards/webby.jpg" alt="Webby Awards" /></a
-      >
-      <a
-        target="_blank"
-        href="https://www.informationisbeautifulawards.com/showcase"
-        ><img
-          src="{assets}/media/awards/iib.png"
-          alt="Information is Beautiful Awards"
-        /></a
-      >
-      <a target="_blank" href="https://lapressclub.org/naej/"
-        ><img
-          src="{assets}/media/awards/la-pc.jpg"
-          alt="Los Angeles Press Club’s National Entertainment Journalism Awards"
-        /></a
-      >
-      <a
-        target="_blank"
-        href="https://opcofamerica.org/Awardarchive/the-whitman-bassow-award-2023/"
-        ><img
-          src="{assets}/media/awards/whitman.jpg"
-          alt="18 The Whitman Bassow Award 2023"
-        /></a
-      >
-      <a target="_blank" href="https://www.youtube.com/watch?v=z2eIfTX-6Jw"
-        ><img
-          src="{assets}/media/awards/fcc.jpg"
-          alt="Kalinga FCC Awards for excellence in Journalism"
-        /></a
-      >
-      <a
-        target="_blank"
-        href="https://www.adobeawards.com/the-gallery?y=2018&liveproject=20188973"
-        ><img
-          src="{assets}/media/awards/adobe.jpg"
-          alt="Adobe Design Achievement Awards"
-        /></a
-      >
-    </Marquee>
+    {#if windowWidth <= 1024}
+      <div class="mobile">
+        <Marquee pauseOnHover="{true}" speed="{15}" play="{play}">
+          {#each awards.slice(0, slice) as item}
+            <a target="_blank" id="{item.id}" href="{item.url}"
+              ><img
+                src="{assets}/media/awards/{item.img}"
+                alt="{item.alt}"
+              /></a
+            >
+          {/each}
+        </Marquee>
+        <Marquee
+          pauseOnHover="{true}"
+          speed="{15}"
+          play="{play}"
+          direction="right"
+        >
+          {#each awards.slice(slice, slice * 2) as item}
+            <a target="_blank" id="{item.id}" href="{item.url}"
+              ><img
+                src="{assets}/media/awards/{item.img}"
+                alt="{item.alt}"
+              /></a
+            >
+          {/each}
+        </Marquee>
+      </div>
+    {:else}
+      <Marquee pauseOnHover="{true}" speed="{45}" play="{play}">
+        {#each awards as item}
+          <a target="_blank" id="{item.id}" href="{item.url}"
+            ><img src="{assets}/media/awards/{item.img}" alt="{item.alt}" /></a
+          >
+        {/each}
+      </Marquee>
+    {/if}
   </Container>
 </section>
 
@@ -84,7 +63,12 @@
   img {
     height: 75px;
     width: auto;
-    margin: var(--space-l-xl);
+    margin: var(--space-l);
+
+    @media (--md-n-below) {
+      height: 50px;
+      margin: var(--space-s);
+    }
   }
 
   a {
@@ -102,5 +86,10 @@
     :global(.marquee) {
       justify-content: space-around;
     }
+  }
+
+  .mobile {
+    display: flex;
+    flex-direction: column;
   }
 </style>
