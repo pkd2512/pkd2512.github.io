1408d56896e54e1bc13d1c93e057b93d3f7d4b35
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2024-07-12 21:58:56 +0100

    refactor: Add swipe nudge to OverflowingImage component

--- src/lib/components/ui/OverflowingImage/index.svelte
+++ src/lib/components/ui/OverflowingImage/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Icon from '@iconify/svelte';
   import Container from '$lib/components/ui/Container/index.svelte';
 
   /**
@@ -42,14 +43,32 @@
    * @type {Number}
    */
   let width;
+
+  $: showNudge = true;
+
+  const handleScroll = (/** @type {any} */ e) => {
+    showNudge = false;
+  };
 </script>
 
 <svelte:window bind:innerWidth="{width}" />
 
 <div class="overflow-img">
+  {#if width < breakpoint && showNudge}
+    <div class="nudge">
+      <Icon
+        icon="ph:hand-swipe-left-bold"
+        width="24"
+        height="24"
+        style="color: white"
+      />
+    </div>
+  {/if}
+
   <figure
     class:shadow="{shadow}"
     style="overflow-x:{width < breakpoint ? 'scroll' : 'auto'}"
+    on:scroll="{handleScroll}"
   >
     <img
       src="{assets}/{url}"
@@ -72,15 +91,42 @@
 <style lang="scss">
   .overflow-img {
     margin-block-end: var(--space-l);
+    position: relative;
 
     figure {
       display: inline-flex;
       margin-block-end: 0;
+      position: relative;
 
       &.shadow {
         box-shadow: var(--shadow-2);
         border: 1px solid var(--gray-soft);
       }
+    }
+  }
+
+  .nudge {
+    position: absolute;
+    top: 30%;
+    right: var(--space-s);
+    z-index: var(--layer-1);
+    background-color: var(--purple-soft);
+    padding: 0.8rem 1rem;
+    border-radius: 50px;
+    animation: swipe 1.5s cubic-bezier(0.86, 0, 0.07, 1) infinite forwards;
+  }
+
+  @keyframes swipe {
+    0% {
+      right: calc(-1 * var(--space-s));
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      right: var(--space-s);
+      opacity: 1;
     }
   }
 </style>
