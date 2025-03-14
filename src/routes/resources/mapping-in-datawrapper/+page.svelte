c602d5577b9ad0a0028d099ef3b5a8da7347bc26
Prasanta Kumar Dutta <7580745+pkd2512@users.noreply.github.com>
2025-03-14 00:15:44 +0000

    add redirect url for notion resources

---
+++ src/routes/resources/mapping-in-datawrapper/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { browser } from '$app/environment';
+  if (browser) {
+    // to prevent error window is not defined, because it's SSR
+    window.location.href =
+      'https://prasantakrdutta.notion.site/Mapping-in-Datawrapper-1ad7996222248012875cc2e44b33c283';
+  }
+</script>
